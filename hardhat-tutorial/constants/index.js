3fe9ca1cb09cd3a5baa38606d3de45244fce5d96
zia <zedlinkus@gmail.com>
2022-04-17 08:38:22 +0100

    edit constant in hardhat folder

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x2F722EEE4B805c9AC4591f5CCce30a387e53eC53";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-six-zeta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
